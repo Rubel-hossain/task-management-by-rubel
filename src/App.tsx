2e798d62e1d00dd54ca3cc223dee3e7761065492
Rubel Hossain <rubel8525@yahoo.com>
2024-11-04 17:39:55 +0600

    App component updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-import ProjectCard from "./components/ProjectCard";
-import EditTaskModal from "./components/EditTaskModal";
-import KanbanBoard from "./components/KanbanBoard";
-import SearchBar from "./components/SearchBar";
 import ProjectManagementDashboard from "./components/ProjectManagementDashboard";
 
 export default function App() {
